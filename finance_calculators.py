01433115085e15e102eed48ecebfbe2591ac2286
Vesna <projectappvesna@gmail.com>
2023-06-05 18:29:30 +0100

    Updated Code

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,73 +1,67 @@
-import math
-
-# calculates simple and compound interest in investments and bond repayments
-
-def total_simple(deposit, rate, length_years):
-    simple_interest = deposit * (1 + rate * length_years)
-    return simple_interest
-
-
-def total_compound(deposit, rate, lenght_years):
-    compound_interest = deposit * math.pow((1 + rate), lenght_years)
-    return compound_interest
+def total_interest(deposit, rate, length_years, interest_type):
+    if interest_type == "simple":
+        interest = deposit * (1 + rate * length_years)
+    elif interest_type == "compound":
+        interest = deposit * pow(1 + rate, length_years)
+    else:
+        return None
+    return round(interest, 2)
 
 
 def bond_repayment(value_house, rate_monthly, num_months):
+    if num_months == 0:
+        print("Error: Please enter a non-zero value for the number of months.")
+        return None
     repayment = (rate_monthly * value_house) / \
-        (1 - (1 + rate_monthly)**(-num_months))
-    return repayment
+        (1 - pow(1 + rate_monthly, -num_months))
+    return round(repayment, 2)
 
 
-print("Welcome to the financial calculator!")
+def main():
+    print("Welcome to the financial calculator!")
+    user_selection = input(
+        "Please enter whether you want an 'Investment' or a 'Bond' calculation:\n > ").lower()
 
-# all user entries are recognized as valid by using the lower method and converting them to lowercase for case-insensitive comparison
-user_selection = input(
-    "Please enter whether you want an 'Investment' or a 'Bond' calculation:\n > ").lower()
+    if user_selection == "investment":
+        print("You have chosen to calculate the interest on your investment.")
+        try:
+            deposit = float(
+                input("Enter the amount of money you are depositing:\n > "))
+            length_years = int(
+                input("Enter the number of years you plan on investing:\n > "))
+            rate = float(input("Enter the rate of interest:\n > ")) / 100
+        except ValueError:
+            print("Error: Please enter a valid numerical value.")
+            return
 
-if (user_selection == "investment"):
-    print("You have chosen to calculate the interest on your investment.")
+        user_interest = input(
+            "Please enter whether you would like to calculate 'Simple' or 'Compound' interest:\n > ").lower()
+        interest = total_interest(deposit, rate, length_years, user_interest)
+        if interest is not None:
+            total_message = f"Based on your choice of '{user_interest.title()} interest' calculation, the total amount is £ {interest}"
+            print(total_message)
 
-    # inputs values from user
-    deposit = float(
-        input("Enter the amount of money you are depositing:\n > "))
-    # interest entered divided by 100
-    length_years = int(
-        input("Enter the number of years you plan on investing:\n > "))
-    rate = float(input("Enter the rate of interest:\n > ")) / 100
+    elif user_selection == "bond":
+        print("You have selected the calculation of the 'Bond repayment' amount.")
+        try:
+            value_house = float(
+                input("Enter the current value of the house:\n > "))
+            rate_monthly = float(
+                input("Enter the interest rate:\n > ")) / 100
+            num_months = int(
+                input("Enter the number of months you plan to take to repay the bond: \n > "))
+        except ValueError:
+            print("Error: Please enter a valid numerical value.")
+            return
 
-    user_interest = input(
-        "Please enter whether you would like to calculate 'Simple' or 'Compound' interest:\n > ").lower()
-
-    # calculate interest simple and compound, and output result
-    if (user_interest == "simple"):
-        simple_interest = total_simple(deposit, rate, length_years)
-        total_message = f"Based on your choice of 'Simple interest' calculation, the total amount is £ {simple_interest:.2f}"
-        print(total_message)
-
-    elif (user_interest == "compound"):
-        compound_interest = total_compound(deposit, rate, length_years)
-        total_message = f"Based on your choice of 'Compound interest' calculation, the total amount is £ {compound_interest:.2f}"
-        print(total_message)
+        repayment = bond_repayment(value_house, rate_monthly, num_months) / 12
+        if repayment is not None:
+            message_bond = f"The monthly bond repayment will be £ {repayment}"
+            print(message_bond)
 
     else:
-        print("Incorrect input. Please enter either 'simple' or 'compound'.")
+        print("Incorrect input. Please enter either 'investment' or 'bond'.")
 
 
-elif (user_selection == "bond"):
-    print("You have selected the calculation of the 'Bond repayment' amount.")
-
-    # inputs value from user
-    value_house = float(input("Enter the current value of the house:\n > "))
-
-    # interest entered divided by 100
-    rate_monthly = float(input("Enter the interest rate:\n > ")) / 100
-    num_months = int(input(
-        "Enter the number of months you plan to take to repay the bond: \n > "))
-
-    # calculate bond repayment and output message and monthly interest divided by 12
-    repayment = bond_repayment(value_house, rate_monthly, num_months) / 12
-    message_bond = f"The monthly bond repayment will be £ {repayment:.2f}"
-    print(message_bond)
-else:
-    # display error message for invalid input
-    print("Incorrect input. Please enter either 'investment' or 'bond'.")
+if __name__ == '__main__':
+    main()
